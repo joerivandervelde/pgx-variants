bdb5863cd3946e045b93ab874b8887b7469dec4e
K. Joeri van der Velde <joeriv@gmail.com>
2020-03-28 10:38:14 +0100

    Update drug to molecule

--- src/main/java/org/molgenis/pgx/tool/data/PgxAllele.java
+++ src/main/java/org/molgenis/pgx/tool/data/PgxAllele.java
@@ -3,11 +3,11 @@ package org.molgenis.pgx.tool.data;
 public class PgxAllele {
     public String allele;
     public String function;
-    public String drug;
+    public String molecule;
 
-    public PgxAllele(String allele, String function, String drug) {
+    public PgxAllele(String allele, String function, String molecule) {
         this.allele = allele;
         this.function = function;
-        this.drug = drug;
+        this.molecule = molecule;
     }
 }
